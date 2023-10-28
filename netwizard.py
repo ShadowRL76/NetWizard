bac6c5add54e5b6b4949f0eb3fce2b04c56eb2bb
ShadowRL76 <rylankass@gmail.com>
2023-10-27 22:12:27 -0400

    netwizard.py

--- netwizard.py
+++ netwizard.py
@@ -188,13 +188,25 @@ class NetWizard():
                 self.print_banner()
                 
     def install_menu(self):
-            self.clear_screen()
-            self.print_banner()
+        self.clear_screen()
+        self.print_banner()
+        try:
             package_name = 'kali-anonsurf'
-            package_dir = '/home/kali-anonsurf'
-            print("Current working directory:", os.getcwd())
-            print("Trying to change to directory:", package_dir)
-            if os.path.exists(package_dir):
+            package = "git clone https://github.com/Und3rf10w/kali-anonsurf.git"
+            package_dir = 'kali-anonsurf'
+            if not os.path.exists(package_dir):
+                user_install = input(f"The package {package_name} is not installed. Would you like to install it? (Y, N)")
+                if user_install == 'Y':
+                    self.clear_screen()
+                    self.print_banner()
+                    home_dir = os.path.expanduser("~")
+                    os.chdir(home_dir)
+                    subprocess.run(package, shell=True, check=True)
+                    os.chdir('kali-anonsurf')
+                    subprocess.run(['./installer.sh'], check=True)
+                else:
+                    self.returnInput()
+            else:
                 try:
                     os.chdir(package_dir)
                     start_command = 'anonsurf start'
@@ -203,18 +215,12 @@ class NetWizard():
                     self.returnInput()
                 except Exception as e:
                     print(f"An error occurred: {e}")
-            else:
-                user_install = input(f"The package {package_name} is not installed. Would you like to install it? (Y, N)")
-                if user_install == 'Y':
-                    self.clear_screen()
-                    self.print_banner()
-                    os.chdir('/home')  
-                    print(f"Current directory before cloning: {os.getcwd()}")
-                    subprocess.run("git clone https://github.com/Und3rf10w/kali-anonsurf.git", check=True)
-                    print(f"Current directory after cloning: {os.getcwd()}")
-                    os.chdir('kali-anonsurf')
-                    print(f"Current directory after changing to kali-anonsurf: {os.getcwd()}")
-                    subprocess.run(['./installer.sh'], check=True)
+
+        except Exception as e:
+            print(f"An error occurred: {e}")
+        print()
+        self.returnInput()
+
                         
             
                 
@@ -319,4 +325,3 @@ if __name__ == '__main__':
     net_wizard = NetWizard()
     net_wizard.menuOptions()
     
-
